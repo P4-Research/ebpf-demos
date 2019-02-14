5ef7a1bbb6ed7a742d5abaaae941d5a6125a5ba1
Ubuntu <osinstom@gmail.com>
2019-02-14 16:18:59 +0000

    Add XDP install script

---
+++ arp-resp/install_xdp.sh
@@ -0,0 +1,3 @@
+clang -Wno-unused-value -Wno-pointer-sign -Wno-compare-distinct-pointer-types -Wno-gnu-variable-sized-type-not-at-end -Wno-tautological-compare -O2 -emit-llvm -g -c arp-resp.c -o -| llc -march=bpf -filetype=obj -o dropper.o
+sudo ip link set dev s1-eth1 xdp off
+sudo ip link set dev s1-eth1 xdp obj dropper.o sec xdp_arp_resp
